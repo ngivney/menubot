d066fe5f8b65cdb3938403e714fa666c99f5b5fc
Nick Givney <ngivney@hotmail.com>
2025-05-11 14:31:45 -0400

    Create menu_data.py
    initial

---
+++ menu_data.py
@@ -0,0 +1,6 @@
+menu_data = {
+    "appetizers": ["Spring Rolls", "Garlic Bread", "Bruschetta", "Mozzarella Sticks"],
+    "main_courses": ["Pizza Margherita", "Spaghetti Bolognese", "Grilled Chicken", "Veg Burger"],
+    "desserts": ["Cheesecake", "Tiramisu", "Ice Cream", "Brownie"],
+    "drinks": ["Coke", "Lemonade", "Water", "Iced Tea"]
+}
